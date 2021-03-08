a7435c3ab201d9aca999c6b1abe52c0bb8d367b5
PolenPoksi <poliksenilapa18@gmail.com>
2021-03-08 12:47:27 +0100

    b&j

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from "react"
 const App = () => {
   const [counter, setCounter] = useState(1)
   const [item, setItem] = useState("")
-   const [items, setItems] = useState([])
+  const [defaultItems, setItems] = useState([])
 
   const handleClick = (e) => {
     e.preventDefault()
@@ -12,45 +12,42 @@ const App = () => {
       todo: item,
       id: counter,
     }
-    setItems( [...items, newTodo])
+    setItems([...defaultItems, newTodo])
     setCounter(counter + 1)
-    console.log(items)
+    console.log(defaultItems)
   }
-  const handleDelete= (e) => {
-    console.log(e.target.id)
+
+  const handleDelete = (e) => {
+    const id = e.target.getAttribute("id")
+    setItems(defaultItems.filter((newToDo) => newToDo.id != id))
+    console.log(defaultItems)
   }
-  
 
   return (
-    <section>
-      <div>
-        <input
-          type='text'
-          name='item'
-          onChange={(event) => setItem(event.target.value)}
-        ></input>
-        <button
-          // type='btn'
-          onClick={handleClick}
-        >
-          Add
-        </button>
-
-        {
-         items.map((e)=>{
-          
-            return (
-              <>
-            <div > {e.todo} </div>
-            <button id={e.id} onClick={handleDelete}> Delete</button>
+    <div className='container'>
+      <input
+        className='inputi'
+        type='text'
+        name='item'
+        onChange={(event) => setItem(event.target.value)}
+      ></input>
+      <button className='buton' onClick={handleClick}>
+        Add
+      </button>
+      <div className='itemsat'>
+        {defaultItems.map((e) => {
+          return (
+            <>
+              <div> {e.todo} </div>
+              <button className='butoniVogel' id={e.id} onClick={handleDelete}>
+                {/* {" "} */}
+                Delete
+              </button>
             </>
-            )
-               
-        })
-          
-          }
+          )
+        })}
       </div>
-    </section>
+    </div>
   )
 }
 
