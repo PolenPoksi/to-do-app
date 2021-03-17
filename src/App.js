73621850414bc294438e96cca24f5999d3b10479
polenpoksi <poliksenilapa18@gmail.com>
2021-03-17 16:25:13 +0100

    ter

--- src/App.js
+++ src/App.js
@@ -1,82 +1,85 @@
-import "./index.css"
-import React, { useState } from "react"
-import Popup from "./PopUp"
-
+import "./index.css";
+import React, { useState } from "react";
+import Popup from "./PopUp";
 
 const App = () => {
-
-  const [counter, setCounter] = useState(1)
-  const [item, setItem] = useState("")
-  const [defaultItems, setItems] = useState([])
+  const [counter, setCounter] = useState(1);
+  const [item, setItem] = useState("");
+  const [defaultItems, setItems] = useState([]);
 
   // this is the popup
 
   const [isOpen, setIsOpen] = useState(false);
+
   const togglePopup = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   const handleClick = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newTodo = {
       todo: item,
       id: counter,
-    }
-    setItems([...defaultItems, newTodo])
-    setCounter(counter + 1)
-    console.log(defaultItems)
-  }
+    };
+    setItems([...defaultItems, newTodo]);
+    setCounter(counter + 1);
+    console.log(defaultItems);
+  };
 
   const handleDelete = (e) => {
-    const id = e.target.getAttribute("id")
-    setItems(defaultItems.filter((newToDo) => newToDo.id != id))
-    console.log(defaultItems)
-  }
+    const id = e.target.getAttribute("id");
+    setItems(defaultItems.filter((newToDo) => newToDo.id != id));
+    console.log(defaultItems);
+  };
 
   return (
-    <div className='container'>
-      <div classname='cont'>
+    <div className="container">
+      <div classname="cont">
         <input
-          className='inputi'
-          type='text'
-          name='item'
+          className="inputi"
+          type="text"
+          name="item"
           onChange={(event) => setItem(event.target.value)}
         ></input>
-        <button className='buton' onClick={handleClick}>
+        <button className="buton" onClick={handleClick}>
           Add
         </button>
       </div>
-      <div className='itemsat'>
-        {defaultItems.map((e) => {
+      <div className="itemsat">
+        {defaultItems.map((item) => {
           return (
             <>
-              <div className='njeitem'>
-                <div> {e.todo} </div>
+              <div className="njeitem">
+                <div> {item.todo} </div>
                 <button
-                  className='butoniVogel'
-                  id={e.id}
+                  className="butoniVogel"
+                  id={item.id}
                   onClick={handleDelete}
-                >Delete
+                >
+                  Delete
                 </button>
-                <button className='butoniVogel' onClick={togglePopup}>Edit{isOpen && <Popup content={
-                  <>
-                    <input
-                      className='inputi'
-                      type='text'
-                      name='item'
-                      onChange={(event) => setItem(event.target.value)}
-                    ></input>
-                  </>
-                }
-                  handleClose={togglePopup} />}
-                </button>
+                <button
+                  className="butoniVogel"
+                  onClick={(item) => {
+                    togglePopup()
+                  }}
+                >
+                  Edit
+                </button>{isOpen && (
+                      <Popup
+                        handleClose={togglePopup}
+                        textEditor={handleClick}
+                        id={item.id}
+                        text={item.todo}
+                      />
+                    )}
               </div>
             </>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/popup.js
+++ src/popup.js
@@ -2,11 +2,18 @@ import React from 'react';
 import "./popup.css";
 
 const Popup = props => {
+  const { handleClose, textEditor, id, text } = props
   return (
     <>
       <div className="popup-box">
         <div className="box">
-          <span className="close-icon" onClick={props.handleClose}>x</span>{props.content}
+          <input
+            className='inputi'
+            type='text'
+            placeholder={text}
+            name='item'
+          ></input>
+          <span className="close-icon" onClick={handleClose, textEditor}>x</span>{props.content}
         </div>
       </div>
     </>
